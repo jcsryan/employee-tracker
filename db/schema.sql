6a955b7c91170be764771b2a0789d6b72e8be434
JCSR <johnconnorsryan@gmail.com>
2020-06-05 01:25:09 -0400

    included npm packages, created tables and seeded them

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,31 @@
+DROP DATABASE IF EXISTS work_db;
+CREATE DATABASE work_db;
+USE work_db;
+
 DROP TABLE IF EXISTS Department;
 DROP TABLE IF EXISTS Role;
 DROP TABLE IF EXISTS Employee;
 
-CREATE TABLE Department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+
+CREATE TABLE department (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Role (
-    id INT PRIMARY KEY,
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT
+    salary DECIMAL(5,0),
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Employee (
-    id INT PRIMARY KEY,
+    id INTEGER (11) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,4 +7,22 @@ VALUES
 
 INSERT INTO Role (title, salary, department_id)
 VALUES
-('data',(5),  )
+('CS lead', 55000, 1 ),
+('CS junior', 38000, 2),
+('CS mid', 44000, 3),
+('3d modeling', 48000, 4),
+('secretary', 32000, 5),
+('CEO', 78000, 6),
+('janitor', 99999, 7),
+('ad man', 60000, 8);
+
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES
+('bob', 'ross', 1, 1),
+('karen', 'matthews', 2, 1),
+('scott', 'tabernackle', 2, 1),
+('frank', 'ocean', 3, 1),
+('baby', 'driver', 4, 2),
+('donald', 'glover', 4, 2),
+('paul', 'nessle', 5, 2);
+
